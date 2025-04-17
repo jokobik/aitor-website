c579519bf7654e4d15bf88b0bb80a90e33352f26
Aitor López <aitor.lopron@gmail.com>
2025-04-17 10:48:15 +0200

    Confirgurar DB para heroku

--- myWeb/myWeb/settings.py
+++ myWeb/myWeb/settings.py
@@ -77,11 +77,17 @@ WSGI_APPLICATION = 'myWeb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3', conn_max_age=600)
 }
 
 
