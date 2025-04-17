7e4e0a5d7525b2bdab4001bb964d31b824e229ae
Aitor López <aitor.lopron@gmail.com>
2025-04-17 11:33:32 +0200

    Actualizar carpeta staticfiles

---
+++ myWeb/staticfiles/js/pdf_viewer.js
@@ -0,0 +1,21 @@
+// scripts.js
+
+const url = "/static/pdfs/curriculum_aitor.pdf";  // Asegúrate de que la ruta sea correcta
+
+const loadingTask = pdfjsLib.getDocument(url);
+loadingTask.promise.then(pdf => {
+    pdf.getPage(1).then(page => {
+        const scale = 1.5;
+        const viewport = page.getViewport({ scale: scale });
+
+        const canvas = document.getElementById('pdf-canvas');
+        const context = canvas.getContext('2d');
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+
+        page.render({
+            canvasContext: context,
+            viewport: viewport
+        });
+    });
+});
